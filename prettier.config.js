7f9e183c87363eed0215aeeb2b2b322ad754f775
Rafael Navarro Canizares Corrêa <rncf1112@gmail.com>
2025-08-19 18:50:35 +0100

    sets up prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+module.exports = config
