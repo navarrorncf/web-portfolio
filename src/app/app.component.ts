fc58823d1ec952f36ea580fb2f510c7fe221e633
Rafael Navarro Canizares Corrêa <rncf1112@gmail.com>
2025-08-21 18:50:40 +0100

    adds header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from './dummy-users';
+import { HeaderComponent } from './components/header/header.component';
 
 type User = {
     id: string;
@@ -13,7 +14,7 @@ type User = {
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: []
+    imports: [HeaderComponent]
 })
 export class AppComponent {
   users: User[] = DUMMY_USERS;
