1980192affe37b2ae61071f7cbff3454ced5220b
Rafael Navarro Canizares Corrêa <rncf1112@gmail.com>
2025-08-21 18:44:08 +0100

    sets up global styles, fonts, dummy data and app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from './dummy-users';
+
+type User = {
+    id: string;
+    name: string;
+    avatar: string;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: []
 })
 export class AppComponent {
-  title = '101'
+  users: User[] = DUMMY_USERS;
+  selectedUser?: User;
 }
