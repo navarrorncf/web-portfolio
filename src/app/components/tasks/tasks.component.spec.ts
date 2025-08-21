11d1d29deb7aed6f9c2af56cd6efd57bd80988b8
Rafael Navarro Canizares Corrêa <rncf1112@gmail.com>
2025-08-21 19:43:51 +0100

    adds tasks component and fixes user.component unit tests

--- src/app/components/tasks/tasks.component.spec.ts
+++ src/app/components/tasks/tasks.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { UserComponent } from './user.component'
+import { TasksComponent } from './tasks.component'
 
-describe('UserComponent', () => {
-  let component: UserComponent
-  let fixture: ComponentFixture<UserComponent>
+describe('TasksComponent', () => {
+  let component: TasksComponent
+  let fixture: ComponentFixture<TasksComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserComponent],
+      imports: [TasksComponent],
     }).compileComponents()
 
-    fixture = TestBed.createComponent(UserComponent)
+    fixture = TestBed.createComponent(TasksComponent)
     fixture.componentRef.setInput('user', { id: 'u1', name: 'User 1', avatar: 'avatar1.png' })
     component = fixture.componentInstance
     fixture.detectChanges()
